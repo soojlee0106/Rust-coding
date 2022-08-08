61ed8dddda32e6746bca39f2c486910c842d4880
Soo Lee <soojlee0106@einsis.com>
2022-08-08 10:16:03 +0900

    Fixes numbers_1.rs

--- leetcode/numbers_1.rs
+++ leetcode/numbers_1.rs
@@ -9,29 +9,29 @@ pub fn minimum_sum(mut num: i32) -> i32 {
 
     fullvec.sort();
 
-    if let &[a1, a2, a3, a4] = fullvec.as_slice() {
-        return 10 * a1 + 10 * a2 + a3 + a4;
-    
-    unreachable!()
-    }
+    let &[a1, a2, a3, a4] = fullvec.as_slice();
+    10 * a1 + 10 * a2 + a3 + a4
 }
 
 // Number of Good Pairs
 pub fn num_identical_pairs(nums: Vec<i32>) -> i32 {
-    let mut pairs= 0;
-    
+    let mut pairs = 0;
+
     for i in 0..nums.len() {
-        for j in i+1..nums.len() {
-            if nums[i]==nums[j] {
-            pairs+=1;
+        for j in i + 1..nums.len() {
+            if nums[i] == nums[j] {
+                pairs += 1;
             }
         }
-    } pairs
+    }
+    pairs
 }
 
 //Kids with the Greatest Number of Candies
 pub fn kids_with_candies(candies: Vec<i32>, extra_candies: i32) -> Vec<bool> {
-    let &greatCandy= candies.iter().max().unwrap();
-    candies.iter().map(|x|x+extra_candies>=greatCandy).collect()
+    let &greatCandy = candies.iter().max().unwrap();
+    candies
+        .iter()
+        .map(|x| x + extra_candies >= greatCandy)
+        .collect()
 }
-
