6c987e61d42216104f910e23b9d6fc016810593a
Soo <soojlee0106@einsis.com>
2022-11-01 09:26:51 +0900

    Fix Merge Sort to use less vectors

--- algorithm/src/merge_sort.rs
+++ algorithm/src/merge_sort.rs
@@ -1,14 +1,14 @@
-fn merge_sort(array: Vec<usize>) -> Vec<usize> {
+fn merge_sort(array: &[usize]) -> Vec<usize> {
     if array.len() < 2 {
         //already sorted
-        return array;
+        return array.to_vec();
     };
 
     let mid_index = array.len() / 2;
-    let lower_array = merge_sort(array[0..mid_index].to_vec());
-    let higher_array = merge_sort(array[mid_index..array.len()].to_vec());
+    let lower_array = merge_sort(&array[0..mid_index]);
+    let higher_array = merge_sort(&array[mid_index..array.len()]);
 
-    let mut merged_array: Vec<usize> = vec![];
+    let mut merged_array: Vec<usize> = Vec::with_capacity(array.len());
     let mut low_index = 0;
     let mut high_index = 0;
 
@@ -21,8 +21,9 @@ fn merge_sort(array: Vec<usize>) -> Vec<usize> {
             high_index += 1;
         }
     }
-    merged_array.append(&mut lower_array[low_index..lower_array.len()].to_vec());
-    merged_array.append(&mut higher_array[high_index..higher_array.len()].to_vec());
+
+    merged_array.extend((&lower_array[low_index..lower_array.len()]).iter());
+    merged_array.extend((&higher_array[high_index..higher_array.len()]).iter());
     merged_array
 }
 
@@ -33,7 +34,7 @@ mod tests {
     #[test]
     fn merge_empty() {
         let array = vec![];
-        let test = merge_sort(array);
+        let test = merge_sort(&array);
         let expected: Vec<usize> = vec![];
         assert_eq!(test, expected);
     }
@@ -41,7 +42,7 @@ mod tests {
     #[test]
     fn merge_simple() {
         let array = vec![5, 3, 1];
-        let test = merge_sort(array);
+        let test = merge_sort(&array);
         let expected: Vec<usize> = vec![1, 3, 5];
         assert_eq!(test, expected);
     }
@@ -49,7 +50,7 @@ mod tests {
     #[test]
     fn merge_complex() {
         let array = vec![5, 9, 3, 8, 2, 1, 4, 7, 6];
-        let test = merge_sort(array);
+        let test = merge_sort(&array);
         let expected: Vec<usize> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9];
         assert_eq!(test, expected);
     }
@@ -57,7 +58,7 @@ mod tests {
     #[test]
     fn merge_complex_large() {
         let array = vec![56, 29, 154, 721, 654, 88, 36, 274, 36, 199, 97];
-        let test = merge_sort(array);
+        let test = merge_sort(&array);
         let expected: Vec<usize> = vec![29, 36, 36, 56, 88, 97, 154, 199, 274, 654, 721];
         assert_eq!(test, expected);
     }
